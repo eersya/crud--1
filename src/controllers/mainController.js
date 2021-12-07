2139ca4126f7f3207c9249240f09e51ae54ec0f6
eersya <eersyaando@gmail.com>
2021-12-06 20:07:47 -0600

    primer commit. Todavía no funciona

---
+++ src/controllers/mainController.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		  res.render('index', { products: products });
+	},
+	  search: (req, res) => {
+        const keywords = req.body.keywords;
+        res.render('index', {
+            products: products.filter(function(p) {
+                p.name == keywords;
+            })
+        });
+	},
+};
+
+module.exports = controller;
