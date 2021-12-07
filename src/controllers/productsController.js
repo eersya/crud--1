1a3cd62b51fe075f971da5af68844522047c0e9d
eersya <eersyaando@gmail.com>
2021-12-06 20:32:09 -0600

    detail añadido

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,39 +8,42 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	// Root - Show all products
-	index: (req, res) => {
-		// Do the magic
-	},
+	  index: (req, res) => {
+        res.render('index', { products: products });
+	  },
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		  const id = req.params.id;
+      product.find(p => p.id === id);
+
+      res.render('detail', { product: product });
 	},
 
-	// Create - Form to create
-	create: (req, res) => {
-		// Do the magic
-	},
-	
-	// Create -  Method to store
-	store: (req, res) => {
-		// Do the magic
-	},
+	  // Create - Form to create
+	  create: (req, res) => {
+		    // Do the magic
+	  },
+	  
+	  // Create -  Method to store
+	  store: (req, res) => {
+		    
+	  },
 
-	// Update - Form to edit
-	edit: (req, res) => {
-		// Do the magic
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		// Do the magic
-	},
+	  // Update - Form to edit
+	  edit: (req, res) => {
+		    // Do the magic
+	  },
+	  // Update - Method to update
+	  update: (req, res) => {
+		    // Do the magic
+	  },
 
-	// Delete - Delete one product from DB
+	  // Delete - Delete one product from DB
 	  destroy : (req, res) => {
         const id = req.body.id;
         let products_new = products.filter(e => e != id);
-	}
+	  }
 };
 
 module.exports = controller;
