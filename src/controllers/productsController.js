a1fee66f6b8044cd43130b72a4732479a290cf54
eersya <eersyaando@gmail.com>
2021-12-06 21:37:38 -0600

    la página ya funciona

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,6 +6,15 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+// Suma los ids para encontrar un id que esté disponible
+function findId(ps) {
+    let id = 0;
+    ps.forEach( function(p) {
+        id = id + p.id;
+    });
+    return id;
+}
+
 const controller = {
 	// Root - Show all products
 	  index: (req, res) => {
@@ -14,37 +23,54 @@ const controller = {
 	// Detail - Detail from one product
 	detail: (req, res) => {
 		  const id = req.params.id;
-      product.find(p => p.id === id);
-	},
-	// Update - Form to edit
-	edit: (req, res) => {
-		let productId = req.params.id;
-		res.send(productId);
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		// Do the magic
-	},
+      product = products.find(p => p.id == id);
 
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-
-      res.render('detail', { product: product });
+      // verificar si existe el producto
+      if (product != null)
+          res.render('detail', { product: product });
+      else
+          console.error('No existe este producto');
 	},
 
 	  // Create - Form to create
 	  create: (req, res) => {
-		    // Do the magic
+        res.render('product-create-form');
 	  },
 	  
 	  // Create -  Method to store
 	  store: (req, res) => {
-		    
+		    const name = req.body.name;
+        const price = req.body.price;
+        const discount = req.body.discount;
+        const category = req.body.category;
+        const description = req.body.description;
+
+        const newProduct = {
+            id: findId(products),
+            name: name,
+            description: description,
+            price: price,
+            discount: price,
+            image: "",
+            category: category
+        };
+
+        // Escribiendo el nuevo producto
+        fs.writeFile(
+            productsFilePath,
+            JSON.stringify(products.push(newProduct)),
+            function(err) {
+                if (err)
+                    throw(err);
+            });
+
+        controller.index(req, res);
 	  },
 
 	  // Update - Form to edit
 	  edit: (req, res) => {
-		    // Do the magic
+		    let productId = req.params.id;
+		    res.send(productId);
 	  },
 	  // Update - Method to update
 	  update: (req, res) => {
